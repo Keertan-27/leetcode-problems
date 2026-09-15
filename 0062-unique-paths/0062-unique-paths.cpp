8978b671e902866169a921e12ac08b3692a36259
Keertan Sharma <159062030+Keertan-27@users.noreply.github.com>
2026-09-16 04:00:17 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.31%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,10 +3,10 @@ public:
     int solve(int i, int j, vector<vector<int>>& dp) {
         if (i < 0 || j < 0)
             return 0;
-        if (i == 0 && j == 0)
-            return 1;
         if (dp[i][j] != -1)
             return dp[i][j];
+        if (i == 0 && j == 0)
+            return 1;
         return dp[i][j] = solve(i - 1, j, dp) + solve(i, j - 1, dp);
     }
     int uniquePaths(int m, int n) {
