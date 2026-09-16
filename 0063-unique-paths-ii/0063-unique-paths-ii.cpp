c0d7b037b0f85448f4f66ad0b568c66207299207
Keertan Sharma <159062030+Keertan-27@users.noreply.github.com>
2026-09-16 19:43:22 +0530

    Time: 0 ms (100%), Space: 11.8 MB (71.12%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<vector<int>>& path,
+              vector<vector<int>>& dp) {
+        if (i < 0 || j < 0)
+            return 0;
+        if (path[i][j] == 1)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i == 0 && j == 0)
+            return 1;
+
+        return dp[i][j] = solve(i - 1, j, path, dp) + solve(i, j - 1, path, dp);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m - 1, n - 1, obstacleGrid, dp);
+    }
+};
